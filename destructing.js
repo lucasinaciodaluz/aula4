60d2b9324c89d0541f69700ef222147583605de1
Lucas Inacio da Luz <lawlithi@gmail.com>
2019-07-06 11:55:12 -0300

    incluido obj cities

--- destructing.js
+++ destructing.js
@@ -3,7 +3,24 @@ const cat = {
   breed: 'Persian',
 };
 
+const cities = {
+  paris: {
+    country: 'France',
+    language: 'French',
+  },
+  brazil: {
+    country: 'Brazil',
+    language: 'Portuguese',
+  },
+};
+
 const { name, breed, age = 3 } = cat;
 // name = Alfred
 // breed = Persian
 // age = 3
+const {
+  brazil: { country },
+  paris,
+} = cities;
+// country = Brazil
+// paris = {country: 'France', language: 'French'}
